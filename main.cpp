bebc52b33a63c02ef1eaf01bc7f7a8b534d14244
Austin Herring <awh44@drexel.edu>
2015-01-12 21:25:17 -0500

    Moved the input files to their own directory

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ int main(void)
 	SlidingBrickPuzzle puzzle;
 
 	std::cout << "Loading SBP-level0.txt:" << std::endl;
-	puzzle.load_game("SBP-level0.txt");
+	puzzle.load_game("input/SBP-level0.txt");
 	puzzle.print_board();
 	std::cout << "is_solved == " << puzzle.is_solved() << std::endl << std::endl;
 
 	std::cout << "Loading SBP-level0-solved.txt" << std::endl;
-	puzzle.load_game("SBP-level0-solved.txt");
+	puzzle.load_game("input/SBP-level0-solved.txt");
 	puzzle.print_board();
 	std::cout << "is_solved == " << puzzle.is_solved() << std::endl << std::endl;
 
